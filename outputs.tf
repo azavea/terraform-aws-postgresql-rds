148a858e4add3555ecbbb85a00a0b5a81e7a1bd5
Olve Sæther Hansen <olve@vimond.com>
2017-06-01 14:52:15 +0200

    Expose hosted_zone_id to enable route53 alias

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "database_security_group_id" {
   value = "${aws_security_group.postgresql.id}"
 }
 
+output "hosted_zone_id" {
+  value = "${aws_db_instance.postgresql.hosted_zone_id}"
+}
+
 output "hostname" {
   value = "${aws_db_instance.postgresql.address}"
 }
